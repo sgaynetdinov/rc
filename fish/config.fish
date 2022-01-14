7438fb9bfd007f4f186bbd4bc6302ba0da6ffce8
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2022-01-14 23:50:38 +0500

    Add config shell fish

---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+if type -q exa
+  alias l "exa -l -g --icons --git --tree --level=1"
+  alias ll "l --level=2"
+  alias lll "l --level=3"
+  alias la "l -a"
+  alias lla "ll -a"
+  alias llla "lll -a"
+end
+
+if type -q git
+  alias g "git"
+end
+
+# start theme startship (install from brew)
+starship init fish | source
